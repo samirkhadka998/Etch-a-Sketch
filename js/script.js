8fd59f018983cf3a85cea292c060e98e7d757844
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-12 18:49:24 +0545

    Made grid to adjust in 3*3 or 100* 100 in same space

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
-const container = document.querySelector(".container");
+createGrid();
 
-const div = document.createElement('div');
 
-div.textContent = 1;
-div.style.height = '100px';
-div.style.width = '100px';
-div.style.display='inline-block';
-div.style.color = 'white';
-div.style.background = 'black';
+const btn = document.querySelector('button');
+btn.addEventListener('click', btnClickPrompt);
 
-container.appendChild(div);
+
+function btnClickPrompt() {
+    let numberOfGrid = prompt("Enter number of grid");
+    if (!Number(numberOfGrid)) {
+        alert("Invalid input.");
+        return;
+    }
+    else if (numberOfGrid > 100 || numberOfGrid < 1) {
+        alert("Number should be between 1 to 100.");
+        return;
+    }
+
+    numberOfGrid = Number(numberOfGrid);
+    createGrid(numberOfGrid);
+}
+
+function createGrid(num = 3) {
+
+    const container = document.querySelector(".container");
+    container.textContent = '';
+    container.style.display = 'Flex';
+    container.style.flexWrap = 'wrap'
+    container.style.flexDirection = 'column'
+    container.style.width = '600px';
+    container.style.height = '600px';
+    container.style.margin = '0 auto'
+
+    for (let index = 1; index <= num; index++) {
+        const row = document.createElement('div');
+        row.classList.add('row')
+        // row.style.display = 'Flex';
+        // // row.style.gap = '10px'
+        // row.style.flexWrap = 'wrap'
+        for (let j = 1; j <= num; j++) {
+            const div = document.createElement('div');
+
+            div.classList.add('rowChild');
+            // div.style.height = '100px';
+            // div.style.width = '100px';
+            // div.style.color = 'white';
+            // div.style.background = 'black';
+
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+
+
+
+    }
+
+}
