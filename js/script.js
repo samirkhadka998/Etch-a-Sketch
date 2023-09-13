ea9e738a725bc33409eab9ef6d63085d7e6229b5
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-13 19:15:10 +0545

    Implemented clear function , enable/disable drawing, color selection

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 
 
+var enableDrawing = true;
+var color = 'random';
 const container = document.querySelector(".container");
-
-
-
-
-
 createGrid();
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', btnClickPrompt);
+
+
+const clearbtn = document.querySelector('#clear');
+clearbtn.addEventListener('click', clearDivs);
+
+const checkbox = document.querySelector("#drawing");
+checkbox.addEventListener("change", onCheckBoxChange);
+
+const colorDropdown = document.querySelector('#color');
+colorDropdown.addEventListener('change', colorChanged);
+
+function onCheckBoxChange(e) {
+    console.log(e.target.checked)
+    if (e.target.checked) {
+        enableDrawing = true;
+    }
+    else {
+        enableDrawing = false;
+
+    }
+}
+
+
 
 
 function btnClickPrompt() {
@@ -34,7 +55,9 @@ function createGrid(num = 16) {
     container.style.flexDirection = 'column'
     container.style.width = '600px';
     container.style.height = '600px';
-    container.style.margin = '0 auto'
+    container.style.margin = '0 auto';
+    container.style.border= "3px solid #ffffff"
+    container.style.background = '#2196f32e'
 
     for (let index = 1; index <= num; index++) {
         const row = document.createElement('div');
@@ -59,16 +82,56 @@ function createGrid(num = 16) {
 
     }
 
+
+    divMouseEnter();
+
+
 }
 
-const divs = document.querySelectorAll('.rowChild');
+function divMouseEnter() {
+    const divs = document.querySelectorAll('.rowChild');
 
-divs.forEach(div => {
-    if(!div) return;
-    div.addEventListener('mouseenter', addTrailingBackground )
-})
+    divs.forEach(div => {
+        if (!div) return;
+        div.addEventListener('mouseenter', addTrailingBackground)
+    })
+}
 
-function addTrailingBackground(e){
-    // this.style.classList.add('black');
-    e.target.classList.add('black');
+
+function clearDivs() {
+    const divs = document.querySelectorAll('.rowChild');
+    if (!divs) return;
+    divs.forEach(div => {
+        if (!div) return;
+        div.style.background = 'none'
+    })
+}
+
+function colorChanged(e){
+    color = e.target.value;
+}
+
+function addTrailingBackground(e) {
+    
+
+    if(enableDrawing){
+        switch (color) {
+            case 'random':
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                randomColor =  "#" + randomColor;
+                e.target.style.background = randomColor;
+                break;
+            case 'black':
+                e.target.style.background = 'black';
+                break;
+            case 'white':
+                e.target.style.background = 'white';
+                break;
+            case 'gray':
+                e.target.style.background = 'gray';
+            default:
+                break;
+        }
+        // e.target.classList.add('black');
+    }
 }
